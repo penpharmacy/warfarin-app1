fb17e6044449ae4a9019774930589d248421b069
penpharmacy <penpharmacypp@gmail.com>
2025-07-21 12:17:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = "warfarin-pwa-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
